e56272889e8276f2799140c14188f78ed98f8899
manojnitt52@gmail.com <manoj@localhost.localdomain>
2015-08-10 00:04:17 +0530

    Change title and Logos

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
 # replace these with your server's information
-set :domain,  "http://52.26.92.224/"
 set :user,    "ubuntu"
 
 # name this the same thing as the directory on your server
 set :application, "thandava-static"
 
 # use your local repository as the source
-set :repository, "file://#{File.expand_path('.')}"
+# set :repository, "file://#{File.expand_path('.')}"
 
 # or use a hosted repository
 #set :repository, "ssh://user@example.com/~/git/test.git"
@@ -19,10 +18,13 @@ ssh_options[:forward_agent] = true
 ssh_options[:auth_methods] = ["publickey"] 
 ssh_options[:keys] = ["/home/manoj/codelab/thandavaEC2.pem"]
 
-set :deploy_via, :copy
-set :copy_exclude, [".git", ".DS_Store"]
 set :scm, :git
+set :repository, "git@github.com:striker03/thandavalasya.git"
 set :branch, "master"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+
+set :copy_exclude, [".git", ".DS_Store"]
 # set this path to be correct on yoru server
 set :deploy_to, "/home/#{user}/public_html/#{application}"
 set :use_sudo, false
